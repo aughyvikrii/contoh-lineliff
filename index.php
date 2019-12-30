6a4080478d92cd3f7f05699a9fe90f8a5d50d343
aughyvikrii <aughyvikrii@gmail.com>
2019-12-30 15:30:32 +0700

    add get profile

--- index.php
+++ index.php
@@ -18,10 +18,19 @@
                     <li><a id="nav-tambah-catatan" href="javascript:void(0);" onclick="gantiMenu('tambah-catatan');">Tambah Catatan</a></li>
                 </ul> -->
             </div>
-
             
             <div class="row">
                 <div class="col-12">
+
+                <div class="alert alert-info">
+                    <p> Open From App : <b id="openFrom"></b> </p>
+                    <p> Is Login : <b id="isLogin"></b> </p>
+                </div>
+
+                <div id="result">
+
+                </div>
+
                     <button class="btn btn-sm btn-success get-profile">Get Profile</button>
                 </div>
             </div>
@@ -29,27 +38,57 @@
 
         <script src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
         <script>
-                // Using a Promise object
-                liff
-                .init({
-                    liffId: "1234567890-abcedfgh" // use own liffId
-                })
-                .then(() => {
-                    // Start to use liff's api
-                })
-                .catch((err) => {
-                    // Error happens during initialization
-                    console.log(err.code, err.message);
-                });
+
+            var init = {};
+
+            // Using a Promise object
+            liff
+            .init({
+                liffId: "1234567890-abcedfgh" // use own liffId
+            })
+            .then(() => {
+                // Start to use liff's api
+            })
+            .catch((err) => {
+                // Error happens during initialization
+                console.log(err.code, err.message);
+            });
+
+            
+
+            var initFirst = function(){
+                init.InClient = liff.isInClient();
+                init.isLogin = liff.isLoggedIn();
+            }
+            initFirst();
+
+            var appendAllView = function(){
+                $('#openFrom').html(''+init.InClient);
+                $('#isLogin').html(''+init.isLogin);
+            }
+
+            appendAllView();
+
+            var setResult = function(type,message)
+            {
+                $('#result').html('');
+                let result = '<div class="alert alert-'+type+'">'+message+'</div>';
+                $('#result').html(result);
+            }
 
             $('.get-profile').click(function(){
-                liff.getProfile()
-                .then( profile => {
-                    console.log(profile)
-                })
-                .catch( (err) => {
-                    console.log('error',err);
-                });
+                if( init.isLogin == true ) {
+                    liff.getProfile()
+                    .then( profile => {
+                        console.log(profile)
+                    })
+                    .catch( (err) => {
+                        console.log('error',err);
+                    });
+                }
+                else {
+                    setResult('danger',"You are not logged in")
+                }
             });
         </script>
     </body>
